25fa3b45800f9216f0c5d406c0c10a9ccb317618
Infance Tony.E <138364215+infance-tony@users.noreply.github.com>
2025-03-30 22:20:05 +0530

    Update Day 1.py

--- Day 1.py
+++ Day 1.py
@@ -2,7 +2,7 @@ Calcium: 280, Iron: 0.1, Fluoride: 0.48, Sulphate: 136, Silica: 74, Bicarbonate:
     H_dosing_tank_capacity: 10, I_dosing_pump: 200, K_plant_water_flow: 9000, J_total_feed_flow: 10000,L_reject_water_flow:54,N_feed_pressure:34,O_RO_delta_P:43,
     M_plant_running_hours: 10, PH: 8.2, no_vessels: 23, RO_membrane_each_vessel: 23, Total_membrane: 79, Membrane_age: 23, Membrane_model: 56, no_stages: 56, water_type: "sea", customer_details: {
         name: "Harish",
-        company: "Green Chemicals",
+        company: "Green Chemicllmmals",
         email: "harish@gmail.com",
         phone: "999876543210",
         date: "26-03-2025"
